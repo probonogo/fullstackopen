7fc5cf6541a4d8a5ce50a803611d4cd8907b4c1a
A. Urquiola <patchamama.go@gmail.com>
2023-11-10 12:08:58 +0100

    Do Not Define Components Within Components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+const Display = (props) => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
 const App = () => {
   const [value, setValue] = useState(10)
 
@@ -9,10 +15,10 @@ const App = () => {
 
   return (
     <div>
-      {value}
-      <button onClick={() => setToValue(1000)}>thousand</button>
-      <button onClick={() => setToValue(0)}>reset</button>
-      <button onClick={() => setToValue(value + 1)}>increment</button>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text='thousand' />
+      <Button handleClick={() => setToValue(0)} text='reset' />
+      <Button handleClick={() => setToValue(value + 1)} text='increment' />
     </div>
   )
 }
