c237c51350dab8203787f9ef5ae7eea978743ae9
A. Urquiola <patchamama.go@gmail.com>
2023-11-12 22:02:25 +0100

    PropTypes

--- part5/src/components/Togglable.jsx
+++ part5/src/components/Togglable.jsx
@@ -1,4 +1,5 @@
 import { useState, forwardRef, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false)
@@ -30,3 +31,7 @@ const Togglable = forwardRef((props, refs) => {
 })
 
 export default Togglable
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+}
