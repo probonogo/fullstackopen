015b7e783e263eae318f33fac1d4bd07fe2e370f
A. Urquiola <patchamama.go@gmail.com>
2023-11-26 21:18:26 +0100

    Finishing the filters

--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-
 import App from './App'
+
 import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+})
+
+const store = createStore(reducer)
+
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
