5acd50954c819dcf143628bb70fb9d56226bd131
A. Urquiola <patchamama.go@gmail.com>
2023-11-27 19:21:04 +0100

    Optimizing the performance

--- part6-query-notes/src/App.jsx
+++ part6-query-notes/src/App.jsx
@@ -5,8 +5,9 @@ const App = () => {
   const queryClient = useQueryClient()
   const newNoteMutation = useMutation({
     mutationFn: createNote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData({ queryKey: ['notes'] })
+      queryClient.setQueryData({ queryKey: ['notes'] }, notes.concat(newNote))
     },
   })
 
@@ -30,6 +31,7 @@ const App = () => {
   const result = useQuery({
     queryKey: ['notes'],
     queryFn: getNotes,
+    refetchOnWindowFocus: false,
   })
 
   console.log(JSON.parse(JSON.stringify(result)))
