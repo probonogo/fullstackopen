2210197515a7c61f9b5857400794c9b41789671d
A. Urquiola <patchamama.go@gmail.com>
2023-12-06 18:28:36 +0100

    Utility Types

--- part9-TypeScript/flight-diaries/src/services/diaryService.ts
+++ part9-TypeScript/flight-diaries/src/services/diaryService.ts
@@ -1,9 +1,18 @@
 import diaries from '../../data/diaries';
 
-import { DiaryEntry } from '../types';
+import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
 };
 
 const addDiary = () => {
@@ -12,5 +21,6 @@ const addDiary = () => {
 
 export default {
   getEntries,
+  getNonSensitiveEntries,
   addDiary,
 };
